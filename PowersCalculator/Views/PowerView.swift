39d6c68247fdc7d62d8f34bdac3576fd2e015972
Russell Gordon <rgordon@lcs.on.ca>
2025-02-11 17:20:05 -0500

    Changed from folders to groups and refactored default view name.

--- PowersCalculator/Views/PowerView.swift
+++ PowersCalculator/Views/PowerView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  PowerView.swift
 //  PowersCalculator
 //
 //  Created by Russell Gordon on 2025-02-11.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct PowerView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    PowerView()
 }

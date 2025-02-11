3de73ae0c65933952049a5f3441c48d45e96fac2
Russell Gordon <rgordon@lcs.on.ca>
2025-02-11 18:00:16 -0500

    Added a simple interface to the view that calculates the square of a given base.

--- PowersCalculator/Views/PowerView.swift
+++ PowersCalculator/Views/PowerView.swift
@@ -8,15 +8,44 @@
 import SwiftUI
 
 struct PowerView: View {
+    
+    // MARK: Stored properties
+    @State var base: Double = 1
+    
+    // MARK: Computed properties
+    var squared: Double {
+        return base * base
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Spacer()
+            
+            HStack(alignment: .top) {
+
+                Text("\(base.formatted())")
+                    .font(.system(size: 96))
+
+                Text("2")
+                    .font(.system(size: 44))
+
+                Text("=")
+                    .font(.system(size: 96))
+
+                Text("\(squared.formatted())")
+                    .font(.system(size: 96))
+            }
+            
+            Stepper(value: $base, step: 1.0, label: {
+                Text("Base")
+            })
+                        
+            Spacer()
         }
         .padding()
     }
+
 }
 
 #Preview {

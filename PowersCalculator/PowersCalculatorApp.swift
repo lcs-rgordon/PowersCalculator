1ae27a66694fa8db27b5d108cc47cdb223d7ac3f
Russell Gordon <rgordon@lcs.on.ca>
2025-02-11 17:17:51 -0500

    Initial Commit

---
+++ PowersCalculator/PowersCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PowersCalculatorApp.swift
+//  PowersCalculator
+//
+//  Created by Russell Gordon on 2025-02-11.
+//
+
+import SwiftUI
+
+@main
+struct PowersCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
